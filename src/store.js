4deb5241669755c9db390eba0ec68384d4f06080
hibo2261 <hborse1@gmail.com>
2023-05-16 15:05:56 +0530

    [Himanshu] : Added Store to the project

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import {  combineReducers,createStore} from "redux"
+import CourseReducer from "./reducers/CourseReducer"
+const mainReducer= combineReducers({
+    CourseReducer
+})
+
+const store=createStore(mainReducer)
+
+export default store
+
+
